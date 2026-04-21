13a7e71c99bda8c0fed85cc4fe3bf50674b37591
Raphael Lima <raphaellima365@gmail.com>
2026-04-20 23:19:55 -0300

    Primeiro commit: Estrutura do projeto e lógica principal

---
+++ src/Funcionario.java
@@ -0,0 +1,29 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+
+public class Funcionario extends Pessoa {
+
+    BigDecimal salario;
+    String funcao;
+
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+        super(nome, dataNascimento);
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        // Remove o símbolo R$ para focar na formatação pedida (milhar ponto, decimal vírgula)
+        String salarioFormatado = nf.format(salario).replace("R$", "").trim();
+
+        return String.format("Nome: %s | Data Nasc: %s | Salário: %s | Função: %s", nome, dataNascimento.format(df), salarioFormatado, funcao);
+    }
+
+}
